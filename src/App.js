579fe080067857b0ecc1812b3d71dc1b67e4cd52
Anjum <ow.um1718@gmail.com>
2024-08-02 10:58:13 +0530

    created gkhead and styled it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import GKHead from "./components/GKHead";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GKHead />
+    </div>
+  );
 }
 
 export default App;
--- src/components/GKHead.js
+++ src/components/GKHead.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function GKHead() {
-  return <div></div>;
+  return (
+    <div className="gkHead-flex">
+      <img
+        id="gklogo"
+        src="./images/cooking logo.jpeg"
+        alt="gklogo"
+        title="gklogo"
+      />
+      <h1 id="gkname">Granny's Kitchen</h1>
+    </div>
+  );
 }
 
 export default GKHead;
