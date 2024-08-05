dc8eafe9d4fa156c90e6f61b51d64813b7b799d4
Anjum <ow.um1718@gmail.com>
2024-08-05 11:57:57 +0530

    styled tagline and added pic quote

--- src/components/GkHome.js
+++ src/components/GkHome.js
@@ -4,7 +4,18 @@ function GkHome() {
   return (
     <div>
       <p>Granny's Kitchen brings you the authentic secrets of healthy life.</p>
-      <p id="">Healthy Inside-Fitness Outside</p>
+      <h1 id="tagline">Healthy Inside-Fitness Outside</h1>
+      <p>
+        Dive into the world of ancient recipes and secrets of my grandma, your
+        grandma, infact every person grandma's secret.
+      </p>
+      <img id="img-resize" src="./images/quote.jpg" alt="" />
+      <p>
+        This blog is an open food-blog to everyone who knows the secret of their
+        ancestor's cooking, cooking process and can upload any secret healthy
+        recipes or tips or superfoods that showcases the authenticity of
+        ancestors wellbeing
+      </p>
     </div>
   );
 }
