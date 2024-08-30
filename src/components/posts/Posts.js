e4231716a29e87d77edb5b0f74a5b016037d08c5
Anjum <ow.um1718@gmail.com>
2024-08-30 18:27:05 +0530

    corrected spelling mistake

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -8,7 +8,7 @@ function Posts() {
       <GKHeader/>
 
       <div id='postSnapShotLayout'>
-        <PostImgSnapShot name="HB Roti" img="./images/HB Roti.jpg" desc="HB Roti ia healthy version of normal chapathi but increases your Hemoglobin(HB) level" path="/post1"/>
+        <PostImgSnapShot name="HB Roti" img="./images/HB Roti.jpg" desc="HB Roti is a healthy version of normal chapathi but increases your Hemoglobin(HB) level" path="/post1"/>
         <PostImgSnapShot name="Immunity Booster" img="./images/Immunity Booster.jpg" desc="These are energy balls which fights with viral infections" path="/post2"/>
         <PostImgSnapShot name="Protein Parantha" img="./images/Protein Parantha.jpg" desc="Heavy and healthy breakfast directly corresponds to happy and energetic day" path="/post3"/>
       </div>        
