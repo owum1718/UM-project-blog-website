2ea832ed335b9dae7b9d7e75e5302c00a38a4e29
Anjum <ow.um1718@gmail.com>
2024-08-30 17:00:51 +0530

    added recipe image

--- src/components/recipes/AddRecipe.js
+++ src/components/recipes/AddRecipe.js
@@ -5,6 +5,8 @@ function AddRecipe() {
   let inref=useRef();
   let navigate=useNavigate();
   return (
+    <div>
+    <button id="arrowRight" onClick={()=>{navigate("/")}}>&#11013;</button>
     <div id='addRecipeLayout'> 
         <form>
           <caption>Recipe Submission</caption>
@@ -13,53 +15,53 @@ function AddRecipe() {
             <legend>Personal information</legend>
 
             <div>
-            <label htmlFor="">Name</label>
-            <input type="text" />
-          </div>
+              <label htmlFor="">Name</label>
+              <input type="text" />
+            </div>
 
-          <div>
-            <label htmlFor="">E-mail</label>
-            <input type="email" />
-          </div>
+            <div>
+              <label htmlFor="">E-mail</label>
+              <input type="email" />
+            </div>
 
-          <div>
-            <label htmlFor="">Phone</label>
-            <input type="tel" />
-          </div>
+            <div>
+              <label htmlFor="">Phone</label>
+              <input type="tel" />
+            </div>
           </fieldset>
 
           <fieldset>
             <legend>Recipe Details</legend>
 
             <div>
-            <label htmlFor="">Recipe Name</label>
-            <input type="text" />
+              <label htmlFor="">Recipe Name</label>
+              <input type="text" />
             </div>
 
             <div>
-            <label htmlFor="">Ingredients</label>
-            <input type="text" name="" id="" />
-            <span ref={inref}></span>
-            <button id='addIng' onClick={()=>{}}>&#10133;</button>
-          </div>
+              <label htmlFor="">Ingredients</label>
+              <input type="text" name="" id="" />
+              <span ref={inref}></span>
+              <button id='addIng' onClick={()=>{}}>&#10133;</button>
+            </div>
 
-          <div>
-            <label htmlFor="">Method</label>
-            <textarea name="" id="" style={{width:"15rem"}}></textarea>
-          </div>
+            <div>
+              <label htmlFor="">Method</label>
+              <textarea name="" id="" style={{width:"15rem"}}></textarea>
+            </div>
 
-          <div id='recipeImgLayout'>
-            <label htmlFor="">Recipe Image</label>
-            <div id='recipeImg'></div>
-            <input type="file" name="" id="" />
-          </div>
+            <div id='recipeImgLayout'>
+              <label htmlFor="">Recipe Image</label>
+              <div id='recipeImg'></div>
+              <input type="file" name="" id="" />
+            </div>
+
+
           </fieldset>
 
           <button type='button' id='recipeSubmit' onClick={()=>{navigate("/congrats")}}>Submit</button>
-
-          
-
         </form>
+    </div>
     </div>
   )
 }
